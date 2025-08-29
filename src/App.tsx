4ad9482f4bdeefad32ef6b6ec65e2ee4b06a5c50
Digvijay <digbjssn@gmail.com>
2025-08-29 17:44:41 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex items-center justify-center'>
+    <p className='m-0 text-5xl font-black border-2 p-5 rounded-3xl'>hello</p>
+    </div>
+  )
+}
+
+export default App
